1e49a807eeae8ef46262b7329ac85309dc9aac76
Chris <75439614+chris-mcnabb@users.noreply.github.com>
2022-06-03 14:45:57 +0200

    init

--- components/icons/Payment.jsx
+++ components/icons/Payment.jsx
@@ -15,8 +15,8 @@ const Payment = () => {
                     fill="currentColor"
                 />
                 <path
-                    fill-rule="evenodd"
-                    clip-rule="evenodd"
+                    fillRule="evenodd"
+                    clipRule="evenodd"
                     d="M4 3C1.79086 3 0 4.79086 0 7V17C0 19.2091 1.79086 21 4 21H20C22.2091 21 24 19.2091 24 17V7C24 4.79086 22.2091 3 20 3H4ZM20 5H4C2.89543 5 2 5.89543 2 7V14H22V7C22 5.89543 21.1046 5 20 5ZM22 16H2V17C2 18.1046 2.89543 19 4 19H20C21.1046 19 22 18.1046 22 17V16Z"
                     fill="currentColor"
                 />
